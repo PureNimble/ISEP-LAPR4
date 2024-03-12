0fadb61ac0b88ec1f4951b05453348f33137e589
Rafael Araújo <1201804@isep.ipp.pt>
2024-03-12 11:56:51 +0000

    ref #5 Updated Scripts
    - Added clean tag to mvn command scripts

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -2,4 +2,4 @@
 ECHO OFF
 ECHO make sure JAVA_HOME is set to JDK folder
 ECHO make sure maven is on the system PATH
-./bin/mvnw $1 dependency:copy-dependencies package
+./bin/mvnw $1 dependency:copy-dependencies clean package

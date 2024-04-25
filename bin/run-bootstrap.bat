eb9e62a07398a0dfa28f831386732bda5dfe1aea
Rafael Araújo <1201804@isep.ipp.pt>
2024-04-25 07:03:37 +0100

    ref #5 Updated Scripts
    - Added demo and smoke to bootstrap

--- bin/run-bootstrap.bat
+++ bin/run-bootstrap.bat
@@ -3,4 +3,4 @@ REM assumes the build was executed with maven copy-dependencies
 SET jobs4u_CP=jobs4u.app.bootstrap\target\jobs4u.app.bootstrap-0.1.0.jar;jobs4u.app.bootstrap\target\dependency\*;
 
 REM call the java VM, e.g, 
-java -cp %jobs4u_CP% lapr4.jobs4u.app.bootstrap.Bootstrap
+java -cp %jobs4u_CP% lapr4.jobs4u.app.bootstrap.Bootstrap %1 -wait

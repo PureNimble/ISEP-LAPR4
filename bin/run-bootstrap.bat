f4d360fb58e7eae7b9610f3e5a627e5c7afd737c
Vasco Sousa <1221700@isep.ipp.pt>
2024-05-02 17:34:18 +0100

    ref #5 Updated Scripts
    - Added plugins jar to bootstrap scripts

--- bin/run-bootstrap.bat
+++ bin/run-bootstrap.bat
@@ -1,6 +1,6 @@
 REM set the class path,
 REM assumes the build was executed with maven copy-dependencies
-SET jobs4u_CP=jobs4u.app.bootstrap\target\jobs4u.app.bootstrap-0.1.0.jar;jobs4u.app.bootstrap\target\dependency\*;
+SET jobs4u_CP=jobs4u.app.bootstrap\target\jobs4u.app.bootstrap-0.1.0.jar;jobs4u.app.bootstrap\target\dependency\*;jobs4u.integrations.plugins.standard\target\integrations.plugins.standard-0.1.0.jar
 
 REM call the java VM, e.g, 
 java -cp %jobs4u_CP% lapr4.jobs4u.app.bootstrap.Bootstrap %1 -wait
--- bin/run-bootstrap.sh
+++ bin/run-bootstrap.sh
@@ -2,7 +2,7 @@
 
 #REM set the class path,
 #REM assumes the build was executed with maven copy-dependencies
-export jobs4u_CP=jobs4u.app.bootstrap/target/jobs4u.app.bootstrap-0.1.0.jar:jobs4u.app.bootstrap/target/dependency/*;
+export jobs4u_CP=jobs4u.app.bootstrap/target/jobs4u.app.bootstrap-0.1.0.jar:jobs4u.app.bootstrap/target/dependency/*;jobs4u.integrations.plugins.standard/target/integrations.plugins.standard-0.1.0.jar
 
 #REM call the java VM, e.g,
 java -cp $jobs4u_CP lapr4.jobs4u.app.bootstrap.Bootstrap $1 -wait

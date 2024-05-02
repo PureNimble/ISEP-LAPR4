14ff344efb5f87b8e02eafd7bf94a8ac5d933282
João Pinto <1221694@isep.ipp.pt>
2024-05-02 21:40:51 +0100

    close #88 Implement test for the major tasks
    - copyFiles_test
    - newFileChecker_test
    - reportFile_test

--- jobs4u.applicationsFileBot/include/info.h
+++ jobs4u.applicationsFileBot/include/info.h
@@ -4,12 +4,12 @@
 #define MAX_FILES 20
 #include "config.h"
 
-typedef struct Files {
+typedef struct Files
+{
     char jobOffer_dir[100];
     int candidateID;
     int numFiles;
     char files[MAX_FILES][100];
-
 
 } Files;
 // Singal setup (setupSignal.c)
@@ -21,13 +21,10 @@ void newFileChecker(Config *config);
 
 // Parent Code (listCandidatesID.c and reportFile.c)
 int listCandidatesID(int *fd, Config *config);
-void reportFile(Config *config,Files* files, int numberOfCandidates);
-void printFilesRecursively(char *basePath, FILE *file);
-
+void reportFile(Config *config, Files *files, int numberOfCandidates);
 
 // Children Code (copyFiles.c)
-void copyFiles(int* send_work_fd,int* recive_work_fd, Config* config);
-char* readFirstLine(char* file_path, int candidateID);
-
+void copyFiles(int *send_work_fd, int *recive_work_fd, Config *config);
+char *readFirstLine(char *file_path, int candidateID);
 
 #endif // INFO_H

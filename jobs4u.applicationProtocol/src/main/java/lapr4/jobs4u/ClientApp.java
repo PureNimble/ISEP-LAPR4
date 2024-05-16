88bf740020b926847053160c4ec3db5221ad717c
Rafael Araújo <1201804@isep.ipp.pt>
2024-05-16 19:44:21 +0100

    ref #129 Application Protocol
    - Added server setting to appsettings
    - Added clientapp class and handler

---
+++ jobs4u.applicationProtocol/src/main/java/lapr4/jobs4u/ClientApp.java
@@ -0,0 +1,21 @@
+package lapr4.jobs4u;
+
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.infrastructure.authz.domain.model.PlainTextEncoder;
+import lapr4.jobs4u.tcpclientserver.TcpClient;
+import lapr4.jobs4u.usermanagement.domain.BasePasswordPolicy;
+import lapr4.jobs4u.infrastructure.persistence.PersistenceContext;
+
+public class ClientApp {
+    public static void main(String[] args) {
+        AuthzRegistry.configure(PersistenceContext.repositories().users(), new BasePasswordPolicy(),
+                new PlainTextEncoder());
+        AppSettings appSettings = new AppSettings();
+
+        boolean isSecure = appSettings.isSSLEnabled();
+        Integer port = appSettings.boardServerPort();
+
+        TcpClient server = new TcpClient(port, ClientHandler.class, isSecure);
+        server.run();
+    }
+}

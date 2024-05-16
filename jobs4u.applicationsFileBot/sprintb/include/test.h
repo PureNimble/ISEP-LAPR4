9ada8521d1e5888b8ab27fcbbbc3afaf6ef2e224
João Pinto <1221694@isep.ipp.pt>
2024-05-17 00:34:30 +0100

    ref #128 Implementation
    - Change ApplicationFileBot Struct so there are two versions: sprintb and sprinc

---
+++ jobs4u.applicationsFileBot/sprintb/include/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+#define TEST_INPUT "./resources/test_folder/input/"
+#define TEST_OUTPUT "./resources/test_folder/output/"
+#define SHARED_FOLDER "./resources/test_folder/"
+#define FILE_PATH_SIZE 200
+#define TEST_CHILDREN 5
+#define TEST_FREQUENCY 5
+#define DELETE_FOLDER_TIME 5
+#include "config.h"
+char *newFileChecker_test(Config config);
+Files copyFiles_test(Config config);
+int reportFile_test(Config config, Files file);
+
+#endif // TEST_H

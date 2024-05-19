c7b879ca440186ac4ee48b46feeb101c8bb0e000
João Pinto <1221694@isep.ipp.pt>
2024-05-19 19:52:48 +0100

    ref #128 Implementation
    - Add a struct to save all information related to the candidate

---
+++ jobs4u.applicationsFileBot/sprintc/include/candidateInfo.h
@@ -0,0 +1,13 @@
+#ifndef CANDIDATE_INFO_H
+#define CANDIDATE_INFO_H
+#define MAX_FILES 20
+typedef struct
+{
+    char jobOffer_dir[100];
+    int candidateID;
+    int numFiles;
+    char files[MAX_FILES][100];
+
+} Files;
+
+#endif // CANDIDATE_INFO_H

a72e5d170984e2729668f0208d5fecdeca5e92ae
João Pinto <1221694@isep.ipp.pt>
2024-05-23 01:04:06 +0100

    ref #122 Implementation
    - UI, Controller created for the US
    - Add new UI to the Customer Manager Menu

---
+++ jobs4u.app.backoffice.console/src/main/java/lapr4/jobs4u/app/backoffice/console/presentation/authz/UploadInterviewUI.java
@@ -0,0 +1,83 @@
+package lapr4.jobs4u.app.backoffice.console.presentation.authz;
+
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.SelectWidget;
+import lapr4.jobs4u.app.backoffice.console.presentation.authz.printer.ApplicationPrinter;
+import lapr4.jobs4u.app.backoffice.console.presentation.authz.printer.JobOpeningPrinter;
+import lapr4.jobs4u.app.common.console.presentation.utils.Utils;
+import lapr4.jobs4u.applicationmanagement.application.ListApplicationsController;
+import lapr4.jobs4u.applicationmanagement.domain.Application;
+import lapr4.jobs4u.applicationmanagement.dto.ApplicationDTO;
+import lapr4.jobs4u.infrastructure.persistence.PersistenceContext;
+import lapr4.jobs4u.interviewmanagement.application.UploadInterviewController;
+import lapr4.jobs4u.interviewmanagement.domain.Interview;
+import lapr4.jobs4u.interviewmanagement.domain.Time;
+import lapr4.jobs4u.jobopeningmanagement.application.ListJobOpeningsController;
+import lapr4.jobs4u.jobopeningmanagement.domain.JobOpening;
+import lapr4.jobs4u.jobopeningmanagement.dto.JobOpeningDTO;
+import lapr4.jobs4u.recruitmentprocessmanagement.domain.Date;
+
+public class UploadInterviewUI extends AbstractUI {
+
+    private final UploadInterviewController controller;
+    private final ListApplicationsController applicationController;
+    private final ListJobOpeningsController jobOpeningsController;
+
+    public UploadInterviewUI() {
+        controller = new UploadInterviewController(PersistenceContext.repositories().interviews(),
+                AuthzRegistry.authorizationService());
+        applicationController = new ListApplicationsController(PersistenceContext.repositories().applications(),
+                AuthzRegistry.authorizationService());
+        jobOpeningsController = new ListJobOpeningsController(PersistenceContext.repositories().jobOpenings(),
+                AuthzRegistry.authorizationService());
+    }
+
+    @Override
+    protected boolean doShow() {
+
+        Application app = selectApplication();
+        if (app == null)
+            return false;
+        //String file = Utils.getPath(false);
+        //checkInterview(); // TODO: LPROG
+
+        //controller.uploadInterview(Interview.valueOf(Date.today().toString(), Time.now().toString(), app));
+        // find interview
+        // add file to interview
+        return false;
+
+    }
+
+    private Application selectApplication() {
+        //TODO: check if theJobOpening interviewPhase is open
+        final Iterable<JobOpeningDTO> jobOpenings = this.jobOpeningsController.filterByCostumerManager();
+        final SelectWidget<JobOpeningDTO> selector = new SelectWidget<>("Job Openings:", jobOpenings,
+                new JobOpeningPrinter());
+        selector.show();
+        final JobOpeningDTO theJobOpeningDTO = selector.selectedElement();
+        if (theJobOpeningDTO == null) {
+            return null;
+        }
+        JobOpening theJobOpening = jobOpeningsController.selectedJobOpening(theJobOpeningDTO);
+        final Iterable<ApplicationDTO> applications = applicationController.filterByJobOpening(theJobOpening);
+        final SelectWidget<ApplicationDTO> selector1 = new SelectWidget<>("Applications:", applications,
+                new ApplicationPrinter());
+        selector1.show();
+        final ApplicationDTO theApplicationDTO = selector1.selectedElement();
+        if (theApplicationDTO == null) {
+            return null;
+        }
+        return applicationController.selectedApplication(theApplicationDTO);
+
+    }
+
+    @Override
+    public String headline() {
+        return "Import Applications";
+    }
+
+    private void checkInterview() {
+
+    }
+}
---
+++ jobs4u.core/src/main/java/lapr4/jobs4u/interviewmanagement/application/UploadInterviewController.java
@@ -0,0 +1,27 @@
+package lapr4.jobs4u.interviewmanagement.application;
+
+import eapli.framework.application.UseCaseController;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import lapr4.jobs4u.interviewmanagement.domain.Interview;
+import lapr4.jobs4u.interviewmanagement.repositories.InterviewRepository;
+import lapr4.jobs4u.usermanagement.domain.BaseRoles;
+
+/**
+ * @author Paulo Gandra de Sousa 2024.04.30
+ */
+@UseCaseController
+public class UploadInterviewController {
+    private final InterviewRepository interviewRepository;
+    private final AuthorizationService authz;
+
+    public UploadInterviewController(InterviewRepository interviewRepository, AuthorizationService authz) {
+        this.interviewRepository = interviewRepository;
+        this.authz = authz;
+    }
+
+    public void uploadInterview(Interview interview) {
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.CUSTOMER_MANAGER, BaseRoles.POWERUSER);
+        interviewRepository.save(interview);
+    }
+
+}

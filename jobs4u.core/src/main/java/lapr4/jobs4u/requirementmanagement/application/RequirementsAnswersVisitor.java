d64ef6df75a3ee4e90d54cbc11e9be76cd1a174e
José Sá <1220612@isep.ipp.pt>
2024-05-27 16:40:14 +0100

    ref #126 Implementation of US-2004
    - Added RequirementsAnswersVisitor

---
+++ jobs4u.core/src/main/java/lapr4/jobs4u/requirementmanagement/application/RequirementsAnswersVisitor.java
@@ -0,0 +1,21 @@
+package lapr4.jobs4u.requirementmanagement.application;
+
+import lapr4.jobs4u.importer.answer.requirement.generated.RequirementsAnswersBaseVisitor;
+import lapr4.jobs4u.importer.answer.requirement.generated.RequirementsAnswersParser;
+
+public class RequirementsAnswersVisitor extends RequirementsAnswersBaseVisitor<String> {
+
+    private String email;
+
+    @Override
+    public String visitStart(RequirementsAnswersParser.StartContext ctx) {
+        visitEmail(ctx.email());
+        return email;
+    }
+
+    @Override
+    public String visitEmail(RequirementsAnswersParser.EmailContext ctx) {
+        email = ctx.getText();
+        return email;
+    }
+}

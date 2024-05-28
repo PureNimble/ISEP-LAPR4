c7677715896c7952a7de889925cdb56279fc7c2e
João Pinto <1221694@isep.ipp.pt>
2024-05-28 22:49:06 +0100

    ref #129 Stoping all servirces when the server is on update Mode

--- bin/update-server.sh
+++ bin/update-server.sh
@@ -10,9 +10,18 @@ echo "Cron job executed at $(date)"
 
 # Check if the output contains "Already up-to-date."
 if [[ $pull != *"Already up-to-date."* ]]; then
+    chmod +x /root/sem4pi-23-24-2di2/bin/*
+    systemctl stop jobs4u
+    systemctl stop smtp
     # Build the project
     ./bin/quickbuild.sh
-    # Restart the server
-    systemctl restart isep
+    # Check the exit status of the previous command
+    if [ $? -eq 0 ]; then
+        echo "->Maven command succeeded"
+        # Restart the server
+        systemctl start jobs4u
+        systemctl start smtp
+    else
+        echo "->Maven command failed"
     fi
 fi

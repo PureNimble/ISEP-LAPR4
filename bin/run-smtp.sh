03c82c2c2835d1b79a758568c0d3429293424257
João Pinto <1221694@isep.ipp.pt>
2024-05-28 21:47:07 +0100

    ref #129 Server Configuration
    - Deploying the SMTP service
    - Making sure to update and restart the TCP service everyday at 00:00

--- bin/run-smtp.sh
+++ bin/run-smtp.sh
@@ -1,1 +1,16 @@
-java -jar fake-smtp-server-2.2.1.jar -Dspring.config.location=/root/sem4pi-23-24-2di2/bin/application.yaml
+#!/usr/bin/env bash
+WORKDIR=/root/sem4pi-23-24-2di2/libs
+PIDFILE=$WORKDIR/smtp.pid
+cd $WORKDIR
+
+# Start the Java application in the background
+java -jar ./fake-smtp-server-2.2.1.jar -Dspring.config.location=./application.yaml &
+
+# Get the PID of the last command executed in the background
+JAVA_PID=$!
+
+# Write the PID to the PID file
+echo $JAVA_PID > $PIDFILE
+
+# Detach from the terminal to ensure the script does not block
+disown
--- bin/update-server.sh
+++ bin/update-server.sh
@@ -10,12 +10,9 @@ echo "Cron job executed at $(date)"
 
 # Check if the output contains "Already up-to-date."
 if [[ $pull != *"Already up-to-date."* ]]; then
-    # If there are changes, check if any of them are in the specific path
-    if [[ $(git diff --name-only HEAD HEAD~1 | grep jobs4u.applicationProtocol) ]]; then
-        # If there are changes in the specific path, run the other script
-        ./bin/quickbuild.sh
-
-        systemctl restart isep
-
+    # Build the project
+    ./bin/quickbuild.sh
+    # Restart the server
+    systemctl restart isep
     fi
 fi

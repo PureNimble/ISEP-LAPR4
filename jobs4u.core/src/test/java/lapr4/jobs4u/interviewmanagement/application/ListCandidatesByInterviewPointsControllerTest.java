409047b4671b2f1663c0457a8a1eb859e1e030d5
Rafael Araújo <1201804@isep.ipp.pt>
2024-05-31 10:54:46 +0100

    ref #112 Tests US-1019
    - Added unit tests

---
+++ jobs4u.core/src/test/java/lapr4/jobs4u/interviewmanagement/application/ListCandidatesByInterviewPointsControllerTest.java
@@ -0,0 +1,68 @@
+package lapr4.jobs4u.interviewmanagement.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import lapr4.jobs4u.interviewmanagement.domain.Interview;
+import lapr4.jobs4u.interviewmanagement.dto.InterviewDTO;
+import lapr4.jobs4u.interviewmanagement.repositories.InterviewRepository;
+import lapr4.jobs4u.jobopeningmanagement.domain.JobOpening;
+import lapr4.jobs4u.usermanagement.domain.BaseRoles;
+
+public class ListCandidatesByInterviewPointsControllerTest {
+
+    @Mock
+    InterviewRepository mockRepo;
+
+    @Mock
+    AuthorizationService mockAuthz;
+
+    @Mock
+    JobOpening mockJobOpening;
+
+    @Mock
+    Interview mockInterview;
+
+    @Mock
+    InterviewDTO mockInterviewDTO;
+
+    ListCandidatesByInterviewPointsController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        when(mockInterview.toDTO()).thenReturn(mockInterviewDTO);
+        when(mockRepo.sortedInterviewsByJobOpening(mockJobOpening, false)).thenReturn(Arrays.asList(mockInterview));
+        controller = new ListCandidatesByInterviewPointsController(mockRepo, mockAuthz);
+    }
+
+    @Test
+    void testAllCandidatesSortedByInterviewPointsForJobOpeningNotNull() {
+        Iterable<InterviewDTO> result = controller.allCandidatesSortedByInterviewPointsForJobOpening(mockJobOpening,
+                false);
+        assertNotNull(result);
+    }
+
+    @Test
+    void testAllCandidatesSortedByInterviewPointsForJobOpeningReturnsExpectedInterviews() {
+        Iterable<InterviewDTO> result = controller.allCandidatesSortedByInterviewPointsForJobOpening(mockJobOpening,
+                false);
+        List<InterviewDTO> resultList = (List<InterviewDTO>) result;
+        assertEquals(mockInterviewDTO, resultList.get(0));
+    }
+
+    @Test
+    void testAllCandidatesSortedByInterviewPointsForJobOpeningCallsEnsureAuthenticatedUserHasAnyOf() {
+        controller.allCandidatesSortedByInterviewPointsForJobOpening(mockJobOpening, false);
+        verify(mockAuthz).ensureAuthenticatedUserHasAnyOf(BaseRoles.CUSTOMER_MANAGER, BaseRoles.POWERUSER);
+    }
+}
---
+++ jobs4u.core/src/test/java/lapr4/jobs4u/interviewmanagement/domain/GradeTest.java
@@ -0,0 +1,37 @@
+package lapr4.jobs4u.interviewmanagement.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
+
+public class GradeTest {
+
+    private final String VALID_GRADE = "75.0";
+    private final String INVALID_GRADE = "-75.0";
+    private final String ANOTHER_INVALID_GRADE = "BOAS";
+
+    @Test
+    public void testGradeCreation() {
+        Grade grade = Grade.valueOf(VALID_GRADE);
+        assertEquals(grade.toString(), VALID_GRADE);
+    }
+
+    @Test
+    public void testGradeCreationWithInvalidEmptyValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Grade.valueOf(INVALID_GRADE);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Grade.valueOf(ANOTHER_INVALID_GRADE);
+        });
+    }
+
+    @Test
+    public void testAddressCreationWithNullValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Grade.valueOf(null);
+        });
+    }
+}

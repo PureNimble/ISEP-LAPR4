b638acd27de2f6e450016c6cb30c9cb3ec3b4b5c
João Pinto <1221694@isep.ipp.pt>
2024-06-07 11:46:50 +0100

    ref #135 3001 Implementation
    - Adding all notifications for the candidates (newApplication,updateState,updateResult)

--- jobs4u.h2/src/main/java/lapr4/jobs4u/TcpH2.java
+++ jobs4u.h2/src/main/java/lapr4/jobs4u/TcpH2.java
@@ -4,7 +4,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
 import lapr4.jobs4u.protocol.ProtocolMessage;
 
 /**
@@ -18,28 +17,33 @@ public class TcpH2 {
     private static DataOutputStream output;
     private static DataInputStream input;
 
-    private static void send(final ProtocolMessage msg) throws IOException {
+    public TcpH2() {
+    }
+
+    private void send(final ProtocolMessage msg) throws IOException {
         output.write(msg.toByteStream());
     }
 
-    private static ProtocolMessage receive() throws IOException, ClassNotFoundException {
+    private ProtocolMessage receive() throws IOException, ClassNotFoundException {
         return ProtocolMessage.fromDataStream(input);
     }
 
-    public static void sendRecv(final ProtocolMessage msg) {
+    public ProtocolMessage sendRecv(final ProtocolMessage msg) {
+        ProtocolMessage response = null;
         try {
             socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
             output = new DataOutputStream(socket.getOutputStream());
             input = new DataInputStream(socket.getInputStream());
             send(msg);
-            receive();
+            response = receive();
             disconnect();
         } catch (final IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+        return response;
     }
 
-    public static void disconnect() throws IOException {
+    public void disconnect() throws IOException {
         output.close();
         input.close();
         socket.close();
--- jobs4u.h2/src/main/java/lapr4/jobs4u/TriggerDataSender.java
+++ jobs4u.h2/src/main/java/lapr4/jobs4u/TriggerDataSender.java
@@ -19,30 +19,21 @@ import java.sql.ResultSet;
 public class TriggerDataSender implements Trigger {
 
     private String tableName;
-    private Integer triggerType;
 
     @Override
     public void init(final Connection conn, final String schemaName, final String triggerName, String tableName,
             final boolean before, final int type) {
-
-        triggerType = switch (tableName) {
-            default -> 1;
-            case "T_RESULT" -> 2;
-            case "T_REQUIREMENT" -> 3;
-            case "T_APPLICATION" -> 4;
-        };
 
         this.tableName = tableName;
     }
 
     @Override
     public void fire(final Connection conn, final Object[] oldRow, final Object[] newRow) {
-        sendToServer(switch (triggerType) {
-            case 1 -> checkState(oldRow, newRow, conn);
-            case 2 -> checkResult(oldRow, newRow, conn);
-            case 3 -> checkRequirements(oldRow, newRow, conn);
-            case 4 -> checkNewApplication(newRow);
-            default -> throw new IllegalArgumentException("Invalid trigger type: " + triggerType);
+        sendToServer(switch (tableName) {
+        default -> checkState(oldRow, newRow, conn);
+        case "T_RESULT" -> checkResult(oldRow, newRow, conn);
+        case "T_REQUIREMENT" -> checkRequirements(oldRow, newRow, conn);
+        case "T_APPLICATION" -> checkNewApplication(newRow);
         });
 
     }
@@ -53,15 +44,19 @@ public class TriggerDataSender implements Trigger {
         final String id = oldRow[2].toString();
 
         List<String> data = new ArrayList<>();
+        String temp = tableName.replace("T_", "").replace("_", "") + "_PK";
 
-        if (!oldStatus.equals(newStatus)) {
-            data.add(oldStatus);
-            data.add(newStatus);
-            String temp = getJobOpeningReference(conn, id);
-            if (temp != null) {
-                data.add(temp);
-            }
-        }
+        final String query = "SELECT CC.ACCOUNT_EMAIL FROM T_RECRUITMENT_PROCESS RP, T_JOB_OPENING JO, T_CUSTOMER_USER CC WHERE  RP."
+                + temp
+                + " = ? AND RP.JOBOPENING_JOBREFERENCE = JO.JOBREFERENCE AND JO.CUSTOMER_CODE = CC.CUSTOMER_CODE ;";
+
+        String email = query(conn, id, query, "ACCOUNT_EMAIL");
+        if (email == null || oldStatus.equals(newStatus))
+            return data;
+
+        String output = "[" + tableName.replace("T_", "") + "] Status changed from " + oldStatus + " to " + newStatus;
+        data.add(email);
+        data.add(output);
 
         return data;
 
@@ -70,94 +65,107 @@ public class TriggerDataSender implements Trigger {
     private List<String> checkResult(final Object[] oldRow, final Object[] newRow, final Connection conn) {
         final String oldResult = oldRow[1].toString();
         final String newResult = newRow[1].toString();
-        final String oldText = oldRow[2] != null ? oldRow[2].toString() : null;
         final String newText = newRow[2] != null ? newRow[2].toString() : null;
-        final String id = oldRow[0].toString();
+        final String id = newRow[0].toString();
+        String output;
         List<String> data = new ArrayList<>();
-        if (!oldResult.equals(newResult)) {
-            data.add(oldResult);
-            data.add(newResult);
-            if (!oldText.equals(newText)) {
-                data.add(oldText);
-                data.add(newText);
-            }
-            String temp = getAplication(conn, id);
-            if (temp != null) {
-                data.add(temp);
-            }
-        }
+
+        String query1 = "SELECT CANDIDATE FROM T_APPLICATION WHERE RESULT_PK = ?;";
+        String query2 = "SELECT JOBOPENING FROM T_APPLICATION WHERE RESULT_PK = ?;";
+        String email = query(conn, id, query1, "CANDIDATE");
+        String jobOpening = query(conn, id, query2, "JOBOPENING");
+
+        if (jobOpening == null || email == null || oldResult.equals(newResult))
+            return data;
+
+        output = "Job Opening[" + jobOpening + "]Result changed from " + oldResult + " to " + newResult;
+
+        if (newText != null)
+            output += " with the following justification: " + newText;
+
+        data.add(email);
+        data.add(output);
         return data;
+
     }
 
     private List<String> checkRequirements(final Object[] oldRow, final Object[] newRow, final Connection conn) {
-        final String ApplicationCode = newRow[2].toString();
+        final String applicationCode = newRow[2].toString();
         final String oldResult = oldRow[3].toString();
         final String newResult = newRow[3].toString();
-        final String oldText = oldRow[5] != null ? oldRow[4].toString() : null;
         final String newText = newRow[5] != null ? newRow[4].toString() : null;
+        List<String> data = new ArrayList<>();
 
-        System.out.println("\nApplicationCode: " + ApplicationCode);
-        System.out.println("Old result: " + oldResult);
-        System.out.println("New result: " + newResult);
-        System.out.println("Old text: " + oldText);
-        System.out.println("New text: " + newText);
-        System.out.println("Table name: " + tableName);
-        return null;
+        final String query = "SELECT CANDIDATE FROM T_APPLICATION WHERE NUMBER = ?;";
+
+        String email = query(conn, newResult, query, "CANDIDATE");
+
+        if (email == null) {
+            System.out.println("Email is null");
+        }
+        if (oldResult.equals(newResult)) {
+            System.out.println("Old result is equal to new result");
+        }
+        /*         if (email == null || oldResult.equals(newResult))
+            return data; */
+
+        String output = "Application[" + applicationCode + "] Requirement changed from " + oldResult + " to "
+                + newResult;
+
+        if (newText != null)
+            output += " with the following justification: " + newText;
+
+        data.add(email);
+        data.add(output);
+
+        return data;
 
     }
 
     private List<String> checkNewApplication(final Object[] newRow) {
-        String email = newRow[3].toString();
-        System.out.println("\nEmail: " + email);
-        return null;
+        final String email = newRow[3].toString();
+        final String jobOpening = newRow[4].toString();
+        final String output = "Your application for Job Opening[" + jobOpening + "] was submitted.";
+        List<String> data = new ArrayList<>();
+        data.add(email);
+        data.add(output);
+
+        return data;
     }
 
-    private String getJobOpeningReference(final Connection conn, final String id) {
-        String jobOpening = null;
-        tableName = tableName.replace("T_", "").replace("_", "") + "_PK";
-        final String query = "SELECT JOBOPENING_JOBREFERENCE FROM T_RECRUITMENT_PROCESS WHERE " + tableName + " = ?";
+    private String query(final Connection conn, final String id, final String query, final String column) {
 
+        String outcome = null;
         try (final PreparedStatement stmt = conn.prepareStatement(query)) {
             stmt.setString(1, id);
 
             try (final ResultSet rs = stmt.executeQuery()) {
                 if (rs.next()) {
-                    jobOpening = rs.getString("JOBOPENING_JOBREFERENCE");
+                    outcome = rs.getString(column);
                 }
             }
         } catch (final SQLException e) {
             e.printStackTrace();
         }
 
-        return jobOpening;
-    }
-
-    private String getAplication(final Connection conn, final String id) {
-        String application = null;
-        final String query = "SELECT NUMBER FROM T_APPLICATION WHERE RESULT_PK = ?;";
-        try (final PreparedStatement stmt = conn.prepareStatement(query)) {
-            stmt.setString(1, id);
-
-            try (final ResultSet rs = stmt.executeQuery()) {
-                if (rs.next()) {
-                    application = rs.getString("NUMBER");
-                }
-            }
-        } catch (final SQLException e) {
-            e.printStackTrace();
-        }
-
-        return application;
+        return outcome;
     }
 
     private void sendToServer(final List<String> data) {
         if (data.isEmpty())
             return;
+        for (String s : data) {
+            System.out.println(s);
+        }
         ProtocolMessage message;
         try {
+            TcpH2 tcpH2 = new TcpH2();
             message = new ProtocolMessage((byte) 1, MessageCode.H2, data.toArray(new String[0]));
-            TcpH2.sendRecv(message);
-            TcpH2.disconnect();
+            ProtocolMessage response = tcpH2.sendRecv(message);
+            if (response != null) {
+                System.out.println(response.toString());
+            }
+            tcpH2.disconnect();
 
         } catch (final IOException e) {
             e.printStackTrace();

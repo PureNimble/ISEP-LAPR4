4c4e9c0cfb4a0f389a8aebcb57effaeecd580c97
João Pinto <1221694@isep.ipp.pt>
2024-06-08 00:34:01 +0100

    ref #151 1014 Tests
    - Make more complex tests

--- jobs4u.core/src/test/java/lapr4/jobs4u/interviewmanagement/domain/TimeTest.java
+++ jobs4u.core/src/test/java/lapr4/jobs4u/interviewmanagement/domain/TimeTest.java
@@ -31,4 +31,44 @@ public class TimeTest {
             Time.valueOf(null);
         });
     }
+
+    @Test
+    public void testTimeCreationWithInvalidFormat() {
+        String invalidFormatTime = "25:00";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Time.valueOf(invalidFormatTime);
+        });
+    }
+
+    @Test
+    public void testTimeCreationWithInvalidMinutes() {
+        String invalidMinutesTime = "10:60";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Time.valueOf(invalidMinutesTime);
+        });
+    }
+
+    @Test
+    public void testTimeCreationWithInvalidHours() {
+        String invalidHoursTime = "24:00";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Time.valueOf(invalidHoursTime);
+        });
+    }
+
+    @Test
+    public void testTimeCreationWithNegativeTime() {
+        String negativeTime = "-01:00";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Time.valueOf(negativeTime);
+        });
+    }
+
+    @Test
+    public void testTimeCreationWithLetters() {
+        String timeWithLetters = "ab:cd";
+        assertThrows(IllegalArgumentException.class, () -> {
+            Time.valueOf(timeWithLetters);
+        });
+    }
 }

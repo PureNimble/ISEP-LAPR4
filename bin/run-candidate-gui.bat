4b4110b445769c5179761e6d8f9b7cd1a0e65870
Rafael Araújo <1201804@isep.ipp.pt>
2024-07-11 21:36:41 +0100

    ref #5 Make scripts for all major tasks
    - Deleted scripts

--- bin/run-candidate-gui.bat
+++
@@ -1,6 +0,0 @@
-REM set the class path,
-REM assumes the build was executed with maven copy-dependencies
-SET jobs4u_CP=..\jobs4u.app.candidate.console\target\jobs4u.app.candidate.console-0.1.0.jar;..\jobs4u.app.candidate.console\target\dependency\*;
-SET jobs4u_MAIN= lapr4.jobs4u.app.candidate.console.CandidateApp
-
-start /D jobs4u.eletron cmd /C npm start %jobs4u_CP% %jobs4u_MAIN%
--- bin/run-customer-gui.bat
+++
@@ -1,6 +0,0 @@
-REM set the class path,
-REM assumes the build was executed with maven copy-dependencies
-SET jobs4u_CP=..\jobs4u.app.customer.console\target\jobs4u.app.customer.console-0.1.0.jar;..\jobs4u.app.customer.console\target\dependency\*;
-SET jobs4u_MAIN= lapr4.jobs4u.app.customer.console.CustomerApp
-
-start /D jobs4u.eletron cmd /C npm start %jobs4u_CP% %jobs4u_MAIN%
